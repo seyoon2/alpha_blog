e410fd3ab2f1c7531c407cfa8b4e4810d4324395
seyoon2 <audtpdbs@gmail.com>
2016-06-28 05:02:55 +0000

    hisasiburi

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,8 @@ class ArticlesController < ApplicationController
  
   
   def create
-         @article = Article.new(article_params)
+         @article = Article.new(article_params) 
+         @article.user = User.first
       if @article.save
          
          flash[:success] = "Article was successfully created"
