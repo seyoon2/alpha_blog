1399207ca32fb7a8f836dd3a9c993957490be6b1
명세윤 <myeongseyoon@myeongseyun-ui-MacBook-Pro.local>
2016-07-05 14:09:05 +0900

    testing

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,16 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'index', to: 'home#index'
   get 'about', to: 'home#about'
-  
+
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
-  
+
+  resources :categories, except: [:destroy]
+
   resources :articles
 
   # The priority is based upon order of creation: first created -> highest priority.
